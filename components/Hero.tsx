c3f662b2bd3e7413fd13ebaa8e3bec3f62e2fc62
Avinash Suthar <avinashsuthar19hmh@gmail.com>
2025-04-30 04:17:49 +0530

    husky

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,20 +7,18 @@ import { BookACall } from "./BookACall";
 const Hero = () => {
   return (
     <section
-      id="home"
+                                  id="home"
       className={`w-full h-screen flex items-center justify-center text-center relative overflow-hidden bg-cover bg-center bg-no-repeat bg-fixed transition-opacity duration-500 ease-in-out bg`}
       style={{ backgroundImage: "url('/images/bg4.webp')" }}
     >
       <div className="relative z-10 text-white  px-4 py-5">
-        <TextGenerateEffect
-          className={cn("text-7xl font-bold")}
-          duration={4}
-          filter={false}
-          words="Innovative Tech Solutions"
-        />
-        <div className="mt-4 h-6">
-
-        </div>
+                        <TextGenerateEffect
+                          className={cn("text-7xl font-bold")}
+                          duration={4}
+                          filter={false}
+                          words="Innovative Tech Solutions"
+                        />
+        <div className="mt-4 h-6"></div>
 
         <TextGenerateEffect
           className={cn("")}
